d314b9f84e1cc3f4131e919b2c320c0ba8ff85a4
Charles Coulton <Charles@charless-air.home>
2019-05-18 10:45:54 -0500

    added insert for start and end times, and method to check to make sure time ranges are valid

--- files/src/babysittertime.py
+++ files/src/babysittertime.py
@@ -1,3 +1,36 @@
 class BabySitterTime:
+    _startTime = None
+    _endTime = None
     def __init__(self):
-        pass
+        self._startTime = 0
+        self._endTime = 0
+
+    @property
+    def StartTime(self):
+        return self._startTime
+
+    @property
+    def EndTime(self):
+        return self._endTime
+
+    def _checkTime(self, startRange, endRange, input):
+        if input >= startRange and input <= 24:
+            return True
+        elif input >= 0 and input <= endRange:
+            return True
+        else:
+            return False
+
+    def StartTime(self, inputTime):
+        if self._checkTime(17, 3, inputTime):
+            self._startTime = inputTime
+            return True
+        else:
+            return False
+
+    def EndTime(self, inputTime):
+        if self._checkTime(18, 4, inputTime):
+            self._endTime = inputTime
+            return True
+        else:
+            return False
