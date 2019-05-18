992882b8038a7325bb122b94d3bb20c9850cc62e
Charles Coulton <Charles@charless-air.home>
2019-05-18 10:12:55 -0500

    added tests for set start and end times

--- files/test/tests.py
+++ files/test/tests.py
@@ -6,6 +6,12 @@ from babysittertime import BabySitterTime
 class BabySitterTimeTest(unittest.TestCase):
     _clock = BabySitterTime()
 
+    def testStartTimeSet(self):
+        self.assertTrue(self._clock.setStartTime())
+
+    def testEndTimeSet(self):
+        self.assertTrue(self._clock.setStartTime())
+
 
 if __name__ == '__main__':
     unittest.main()
