e6c868c0a4c07431075cb8f11d8822cce4c1e903
Charles Coulton <Charles@charless-air.home>
2019-05-18 13:11:04 -0500

    making more tests, and refacting current tests to be more robust

--- files/test/tests.py
+++ files/test/tests.py
@@ -7,23 +7,35 @@ class BabySitterTimeTest(unittest.TestCase):
     _clock = BabySitterTime()
 
     def testStartTimeSet(self):
-        self.assertTrue(self._clock.StartTime(17))
-        self.assertFalse(self._clock.StartTime(16))
+        for index in xrange(0, 24):
+            if (index >= 0 and index <= 3) or (index >= 17 and index <=24):
+                self.assertTrue(self._clock.StartTime(index))
+            else:
+                self.assertFalse(self._clock.StartTime(index))
 
     def testEndTimeSet(self):
-        self.assertTrue(self._clock.EndTime(4))
-        self.assertFalse(self._clock.EndTime(5))
+        for index in xrange(0, 24):
+            if (index >= 0 and index <= 4) or (index >= 18 and index <= 24):
+                self.assertTrue(self._clock.EndTime(index))
+            else:
+                self.assertFalse(self._clock.EndTime(index))
 
     def testValidTimesSet(self):
         self.assertFalse(self._clock.StartTime(25))
         self.assertFalse(self._clock.EndTime(25))
+        self.assertFalse(self._clock.StartTime(-1))
+        self.assertFalse(self._clock.EndTime(-1))
 
     def testTimesAreValid(self):
         self.assertTrue(self._clock.CreateShift(17, 4))
 
-    def testTimesAreNotValid(self):
+    def testTimesAreInsideWorkHours(self):
         self.assertFalse(self._clock.CreateShift(16, 4))
         self.assertFalse(self._clock.CreateShift(17, 5))
+
+    def testTimeOrder(self):
+        self.assertTrue(self._clock.CreateShift(19,20))
+        self.assertFalse(self._clock.CreateShift(20,19))
 
 if __name__ == '__main__':
     unittest.main()
