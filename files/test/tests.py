b8c247109a54c225c82e2f537580266dd5bd0fc9
Charles Coulton <Charles@charless-air.home>
2019-05-18 10:32:07 -0500

    adding tests to fail for times outsife of accepted ranges

--- files/test/tests.py
+++ files/test/tests.py
@@ -7,10 +7,12 @@ class BabySitterTimeTest(unittest.TestCase):
     _clock = BabySitterTime()
 
     def testStartTimeSet(self):
-        self.assertTrue(self._clock.setStartTime())
+        self.assertTrue(self._clock.StartTime(17))
+        self.assertFalse(self._clock.StartTime(16))
 
     def testEndTimeSet(self):
-        self.assertTrue(self._clock.setStartTime())
+        self.assertTrue(self._clock.EndTime(4))
+        self.assertFalse(self._clock.EndTime(5))
 
 
 if __name__ == '__main__':
