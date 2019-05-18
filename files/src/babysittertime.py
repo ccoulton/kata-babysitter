2211f1694d49d468eb32fd0d9477d8150bdc27bb
Charles Coulton <Charles@charless-air.home>
2019-05-18 12:18:22 -0500

    Making the UI expandable, making the exceptions be more robust and have a generic error state.  Added Family payschedules,

--- files/src/babysittertime.py
+++ files/src/babysittertime.py
@@ -1,4 +1,14 @@
+'''
+- Family A pays $15 per hour before 11pm, and $20 per hour the rest of the night
+- Family B pays $12 per hour before 10pm, $8 between 10 and 12, and $16 the rest of the night
+- Family C pays $21 per hour before 9pm, then $15 the rest of the night
+'''
 class BabySitterTime:
+    #fam,from 5-6,  6-7,  7-8,   8-9,   9-10,  10-11, 11-12, 12-1,  1-2, 2-3,  3-4
+    _famA = {17:15, 18:15, 19:15, 20:15, 21:15, 22:15, 23:20, 0:20, 1:20, 2:20, 3:20}
+    _famB = {17:12, 18:12, 19:12, 20:12, 21:12, 22:8,  23:8,  0:16, 1:16, 2:16, 3:16}
+    _famC = {17:21, 18:21, 19:21, 20:21, 21:15, 22:15, 23:15, 0:15, 1:15, 2:15, 3:15}
+    _famDict = {'A':_famA, 'B':_famB, 'C':_famC}
     _startTime = None
     _endTime = None
     def __init__(self):
@@ -59,18 +69,19 @@ def main():
     shifts = {}
     shifts[0] = BabySitterTime()
     print("Times to be inserted in 24 hour clock times, rounded to nearest hour")
-    print("ignoring mintues.")
-    while not shifts[0].ValidShift():
-        try:
-            temp_start = int(input("Please enter your start time: "))
-            temp_end = int(input("Please enter your end time: "))
-            shifts[0].CreateShift(temp_start, temp_end)
-        except NameError:
-            print("A value you entered was not a whole number")
-        except:
-            print("Something went wrong please report this to:")
-            print("https://github.com/ccoulton/kata-babysitter")
-    print("Thank You for your input")
+    print("ignoring mintues. i.e. 5pm is 17, 11pm is 23, and 3am is 3")
+    for shift in shifts.values():
+        while not shift.ValidShift():
+            try:
+                temp_start = int(input("Please enter your start time: "))
+                temp_end = int(input("Please enter your end time: "))
+                shifts[0].CreateShift(temp_start, temp_end)
+            except NameError:
+                print("A value you entered was not a whole number")
+            except:
+                print("Something went wrong please report this to:")
+                print("https://github.com/ccoulton/kata-babysitter")
+        print("Thank You for your input")
 
 if __name__ == '__main__':
     main()
